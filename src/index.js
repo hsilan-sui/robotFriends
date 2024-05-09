6ec6f239b8079f09921c57ab4bd2c2705c9dce2a
hsilan-sui <suihsilan1101@icloud.com>
2024-05-09 16:26:01 +0800

    create App component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import React from "react"; //React套件 view library
 //react可以不限於使用在apps還有mobile以及vr等等 所以會需要分開載入上述兩個套件原因，而react-dom/client適用於Dom web
 import ReactDOM from "react-dom";
 import "./index.css";
-import CardList from "./CardList";
-// import App from "./App";
+import App from "./App";
 import "tachyons";
-import { robots } from "./robots";
 import reportWebVitals from "./reportWebVitals";
 
 //ReactDOM.render(): 這是 React DOM 提供的一個方法，它用來將 React 元素渲染到 DOM 中。這個方法需要兩個參數：第一個參數是要渲染的 React 元素，第二個參數是指定渲染目標的 DOM 元素
-ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
