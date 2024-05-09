ba9f6318be649a8213895d362d3bf3420a836ea7
hsilan-sui <suihsilan1101@icloud.com>
2024-05-09 16:57:03 +0800

    change App component to class component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>Hello RobotFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+//在one way data flow情況下，要如何讓子元件彼此響應當前“狀態”進行互動？＝> state /memory > props
+//class component寫法
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="tc">
+        <h1>Hello RobotFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div className="tc">
+//       <h1>Hello RobotFriends</h1>
+//       <SearchBox />
+//       <CardList robots={robots} />
+//     </div>
+//   );
+// };
 
 export default App;
