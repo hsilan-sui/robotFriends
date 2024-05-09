26df168ed351df9a7a4241cd75e73e882d1ffb02
hsilan-sui <suihsilan1101@icloud.com>
2024-05-09 17:26:49 +0800

    set state in App class, set onChange in SearchBox component, make SearchBox and CardList data state communicate

--- src/App.js
+++ src/App.js
@@ -7,12 +7,40 @@ import { robots } from "./robots";
 //class component寫法
 
 class App extends React.Component {
+  constructor() {
+    super(); //js 子类拥有自己的构造函数 在子类的构造函数中需要使用 super() 来调用父类的构造函数(這裡是調用React.Component)
+    //設置state
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  //
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+    // console.log(event.target.value);
+    //想讓searchinput的值和robots的資料做互動=>filter 並在渲染頁面時assign進來
+    // const filteredRobotsArr = this.state.robots.filter((robot) => {
+    //   return robot.name
+    //     .toLowerCase()
+    //     .includes(this.state.searchfield.toLowerCase());
+    // });
+    // console.log(filteredRobotsArr);
+  };
+
   render() {
+    //想讓searchinput的值和robots的資料做互動=>filter 並在渲染頁面時assign進來
+    const filteredRobotsArr = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1>Hello RobotFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobotsArr} />
       </div>
     );
   }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
+  // const { searchChange } = props;
   return (
     <div className="pa2">
       <input
         type="search"
         placeholder="search robots"
         className="pa3 ba b--light-pink bg-lightest-blue"
+        onChange={searchChange}
       />
     </div>
   );
